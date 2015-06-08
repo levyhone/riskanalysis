6858b271a9e65217dff75d382706baa7a9539265
levyhone <levyhone@uw.edu>
2015-06-08 12:37:21 -0500

    jsut a trial

--- ui.R
+++ ui.R
@@ -1,12 +1,9 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
+
   titlePanel("Hello Shiny!"),
   
-  # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
       sliderInput("bins",
@@ -16,7 +13,6 @@ shinyUI(fluidPage(
                   value = 30)
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
       plotOutput("distPlot")
     )
